86a32ed211e17c7194e6c5b97ce87058ab170148
Sahad.p <59341122+sahadz@users.noreply.github.com>
2025-11-22 20:59:35 +0530

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,22 @@
+import { useState } from 'react'
+import { createPagesBrowserClient } from '@supabase/auth-helpers-nextjs'
+import { SessionContextProvider } from '@supabase/auth-helpers-react'
 import '../styles/globals.css'
 import Layout from '../components/Layout'
-export default function App({Component, pageProps}) {
+
+export default function App({ Component, pageProps }) {
+  // Create a new supabase browser client for every page load
+  const [supabaseClient] = useState(() => createPagesBrowserClient())
+
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    // This provider is CRITICAL for server-side auth to work
+    <SessionContextProvider
+      supabaseClient={supabaseClient}
+      initialSession={pageProps.initialSession}
+    >
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </SessionContextProvider>
   )
 }
